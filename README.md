bdb4647a7960da1c8627c907c21c76e9259b45b9
memiah-steve <steve@memiah.co.uk>
2016-03-17 11:15:36 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,150 @@
+MySQL S3 Backup
+===============
+
+An Ansible Role that installs aws-cli and configures a bash script to
+upload individual MySQL database backups to Amazon S3.
+
+Requirements
+------------
+
+The role assumes you already have MySQL installed.
+
+If enabling GPG, you will need to have created those credentials too.
+
+You will need an Amazon AWS account, with a user and access key security
+credentials with permission to the specified bucket.
+
+It is recommended that you create a user with locked down permissions.
+Below is a policy named `AmazonS3CreateReadWriteAccess-<bucket-name>`
+that can be used to provide limited (create/list/put) access to the
+bucket `<bucket-name>`.
+
+    {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Action": [ "s3:CreateBucket", "s3:ListBucket" ],
+                "Resource": [ "arn:aws:s3:::<bucket-name>" ]
+            },
+            {
+                "Effect": "Allow",
+                "Action": [ "s3:PutObject" ],
+                "Resource": [ "arn:aws:s3:::<bucket-name>/*" ]
+            }
+        ]
+    }
+
+Role Variables
+--------------
+
+Available variables are listed below, along with default values (see 
+`defaults/main.yml`):
+
+    mysql_backup_dir: "/opt/mysql-s3-backup"
+
+Directory where the backup script and config will be stored.
+
+    mysql_backup_cron:
+      enabled: true
+      hour: 1
+      minute: 0
+
+Cron is enabled and set to run at at 01:00 every day.
+
+    mysql_backup_aws_profile: "mysql-s3-backup"
+
+Fro separation we create a new AWS profile for this script context.
+
+    mysql_backup_aws_access_key: "<accesss-key>"
+    
+Your Amazon AWS access key.
+
+    mysql_backup_aws_secret_key: "<secret-key>"
+    
+Your Amazon AWS secret key.
+
+    mysql_backup_aws_region: eu-west-1
+    
+Region name where the S3 bucket is located.
+
+    mysql_backup_aws_format: text
+    
+Format of command output.
+
+    mysql_backup_config: []
+
+Customisations to the backup script itself (values use bash syntax).
+
+* `timestamp: "$(date +"%Y-%m-%d_%H.%M.%S")"`
+  Date and time used to create backup directories "YYYY-MM-DD_HH.II.SS".
+* `backup_dir: "/tmp/mysql-s3-backups/${timestamp}"`
+  Local backup directory path used to store files prior to upload.
+* `backup_dir_remove: "true"`
+  Remove local backup directory upon script completion.
+* `aws_bucket: "mysql-s3-backups"`
+  Default AWS S3 bucket name.
+* `aws_dir: "$timestamp"`
+  Default AWS directory to store backups.
+* `aws_enabled: "true"`
+  Enable upload to Amazon S3, if false the "backup_dir_remove" flag will be set to false.
+* `aws_profile: "mysql-s3-backup"`
+  Set the AWS profile to use (~/.aws/credentials and ~/.aws/config).
+* `mysql_slave: "false"`
+  Set to true if backing up from a MySQL slave server, this will stop the slave and start it again when the script is finished.
+* `mysql_use_defaults_file: "true"`
+  Use default MySQL config file.
+* `mysql_defaults_file: ""`
+  Specifically set the defaults file location, e.g. "/etc/my.cnf".
+* `mysql_user: ""`, `mysql_password: ""`, `mysql_host: ""`
+  If mysql_use_defaults_file is false, it will attempt to use these parameters to connect to MySQL.
+* `mysql_exclude: "information_schema|performance_schema|mysql|sys"`
+  List of databases to exclude from the export, this is a pipe separated list (RegEx).
+* `mysqldump_args: "--triggers --routines --force --opt --add-drop-database"`
+  Default flags used for database dump.
+* `gpg_enabled: "false"`
+  If true, backups will be encrypted with GPG and the suffix ".gpg" added to each of the backup files.
+* `gpg_args: "--encrypt --batch --trust-model always"`
+  Default flags used for GPG encryption.
+* `gpg_recipient: ""`
+  Recipient of the public-key encrypted files
+* `gpg_sign: "false"`
+  Flag to enforce signing of the backups.
+* `gpg_signer: ""`
+  Default GPG key to sign with.
+
+Dependencies
+------------
+
+None.
+
+Example Playbook
+----------------
+
+Including an example of how to use your role (for instance, with 
+variables passed in as parameters) is always nice for users too:
+
+    - hosts: mysql-servers
+      vars_files:
+        - vars/main.yml
+      roles:
+         - { role: memiah.mysql-s3-backup }
+
+*Inside `vars/main.yml`*:
+
+    mysql_backup_aws_access_key: "<accesss-key>"
+    mysql_backup_aws_secret_key: "<secret-key>"
+    mysql_backup_config:
+      aws_profile: "{{ mysql_backup_aws_profile }}"
+      aws_bucket: "bash-mysql-s3-backup-test"
+      backup_dir: "{{ mysql_backup_dir }}/backups/${timestamp}"
+
+License
+-------
+
+MIT / BSD
+
+Author Information
+------------------
+
+This role was created in 2016 by [Memiah Limited](https://github.com/memiah).
---
+++ files/backup.sh
@@ -0,0 +1,310 @@
+#!/usr/bin/env bash
+# Export MySQL databases into individual backup files and upload them
+# to Amazon S3 (with optional GPG encryption).
+
+# Path to config file to allow variables to be overridden.
+readonly config_file="backup.cfg"
+
+# Date and time used to create backup directories "YYYY-MM-DD_HH.II.SS".
+timestamp=$(date +"%Y-%m-%d_%H.%M.%S")
+# Local backup directory path used to store files prior to upload.
+backup_dir="/tmp/mysql-s3-backups/${timestamp}"
+# Remove local backup directory upon script completion (true or false).
+backup_dir_remove=true
+# Default file extension for exported databases.
+file_extension=".sql.gz"
+# Lock file directory.
+lock_dir="/tmp/mysql-s3-backup-lock"
+# PID file.
+pid_file="$lock_dir/pid"
+
+# Auto lookup path to mysql.
+mysql_cmd=$(which mysql)
+# Set to true if backing up from a MySQL slave server, this will stop the slave
+# and start it again when the script is finished.
+mysql_slave=false
+# Use default MySQL config file.
+mysql_use_defaults_file=true
+# Specifically set the defaults file location, e.g. "/etc/my.cnf".
+mysql_defaults_file=""
+# If mysql_use_defaults_file is false, it will attempt to use the following
+# parameters to connect to MySQL.
+mysql_user=""
+mysql_password=""
+mysql_host=""
+# List of databases to exclude from the export, this is a pipe separated list.
+mysql_exclude="information_schema|performance_schema|mysql|sys"
+
+# Auto lookup path to mysqladmin, used to start / stop slave.
+mysqladmin_cmd=$(which mysqladmin)
+# Auto lookup path to mysqldump.
+mysqldump_cmd=$(which mysqldump)
+# Default flags used for database dump.
+mysqldump_args="--triggers --routines --force --opt --add-drop-database"
+
+# Auto lookup path to gpg.
+gpg_cmd=$(which gpg)
+# If true, backups will be encrypted with GPG and the suffix ".gpg" added to
+# each of the backup files.
+gpg_enabled=false
+# Default flags used for GPG encryption.
+gpg_args="--encrypt --batch --trust-model always"
+# Recipient of the public-key encrypted files
+gpg_recipient=""
+# Flag to enforce signing of the backups.
+gpg_sign=false
+# Default GPG key to sign with.
+gpg_signer=""
+
+# Default compress command, this must be set but can be changed.
+compress_cmd="gzip -c"
+
+# Auto lookup path to aws command.
+aws_cmd=$(which aws)
+# Enable upload to Amazon S3, if this is disabled, the "backup_dir_remove"
+# flag will be set to false.
+aws_enabled=true
+# Set the AWS profile to use (~/.aws/credentials and ~/.aws/config)
+aws_profile="mysql-s3-backup"
+# Default AWS S3 bucket name.
+aws_bucket="mysql-s3-backups"
+# Default AWS directory to store backups.
+aws_dir="$timestamp"
+
+# Set traps for specific signals.
+trap finish EXIT
+trap forced_cleanup SIGHUP SIGINT SIGTERM SIGQUIT SIGUSR1
+
+# Load overrides from external config file.
+if [ -f "$config_file" ]; then
+  . $config_file
+fi
+
+# Force cleanup if script was terminated.
+function forced_cleanup {
+  message "error" "Script terminated"
+  cleanup
+  exit 1
+}
+
+# Tidy up after the script is finished, restarting the slave and removing
+# the local backup dir if necessary.
+function cleanup {
+  printf "[Cleanup]\n"
+  # Restart slave if required.
+  if [ "$mysql_slave" == "true" ]; then
+     printf "Restart slave ... "
+     if [ "$mysql_slave_restart" == "true" ]; then
+
+       "$mysqladmin_cmd" "$mysql_args" start-slave
+       success_or_error
+     else
+       message "warn" "Skipped"
+     fi
+  fi
+  # Tidy up files by deleting the backup directory.
+  printf "Deleting local backup directory ... "
+  if [ "$backup_dir_remove" == "true" ]; then
+    rm -rf "$backup_dir"
+    success_or_error
+  else
+    message "warn" "Skipped"
+  fi
+  # Remove the lock dir.
+  printf "Removing lock file ... "
+  rm -rf "$lock_dir"
+  success_or_error
+}
+
+# Output a styled mini success or error message.
+function message {
+  icon="✘"
+  case "$1" in
+    "success") colour=32; icon="✔"; message="Done" ;;
+    "warn") colour=33; message="Warning" ;;
+    *) colour=31; message="Error" ;;
+  esac
+  if [ "$2" ]; then message="$2"; fi
+  printf "\e[0;${colour}m${icon} ${message}\e[0m\n"
+}
+
+# Display the script run time when it finishes.
+function finish {
+  local h=$(($SECONDS/3600))
+  local m=$(($SECONDS%3600/60))
+  local s=$(($SECONDS%60))
+  printf "[Finished: %dh %dm %ds]\n" $h $m $s
+}
+
+# Stop the script, usually due to an error, triggers exit trap.
+function quit {
+  if [ "$1" ]; then message "error" "$1"; fi
+  cleanup
+  exit 1
+}
+
+# Display success or error message based on cmd response.
+function success_or_error {
+  if [ $? -eq 0 ]; then message "success"; else message "error"; fi
+}
+
+# Display success or error message based on cmd response.
+function success_or_quit {
+  if [ $? -eq 0 ]; then message "success"; else quit "error"; fi
+}
+
+# Check if the script is already running.
+mkdir -p "$lock_dir" 2> /dev/null
+if [ $? -eq 0 ]; then
+  # Write process ID (PID) to pid_file.
+  echo $$ > $pid_file
+else
+  quit "Backup is already running. ($pid_file)"
+  exit 6
+fi
+
+# If GPG is enabled, check the package was found.
+if [ "$gpg_enabled" == "true" ]; then
+  if [ -z "$gpg_cmd" ]; then
+    quit "GPG package not found, install gpg or set gpg_enabled to false."
+  fi
+fi
+
+# If s3 upload is enabled, check for s3cmd package.
+if [ "$aws_enabled" = "true" ]; then
+  if [ -z "$aws_cmd" ]; then
+    quit "AWSCli package not found, install awscli or set aws_enabled to false."
+  fi
+fi
+
+# Create backup dir if it does not exist.
+if [ ! -d "$backup_dir" ]; then
+  mkdir -p "$backup_dir"
+  if [ $? -ne 0 ]; then
+    quit "Failed to create backup directory ${backup_dir}.";
+  fi
+fi
+
+# Build mysql args list based on the defined config options (defaults file or
+# user and password combination).
+mysql_args_array=();
+if [ "$mysql_use_defaults_file" == "true" ]; then
+  if [ ! "$mysql_defaults_file" ]; then
+    mysql_args_array+=("--defaults-file=${mysql_defaults_file}")
+  fi
+else
+  if [ "$mysql_user" != "" ]; then
+    mysql_args_array+=("--user=${mysql_user}")
+  fi
+  if [ "$mysql_password" != "" ]; then
+    mysql_args_array+=("--password=${mysql_password}")
+  fi
+  if [ "$mysql_host" != "" ]; then
+    mysql_args_array+=("--host=${mysql_host}")
+  fi
+fi
+
+# Join the MySQL args by single space
+mysql_args=$(echo "${mysql_args_array[*]}")
+
+echo "[Start MySQL Export]"
+
+# If the slave flag is set, check if it is currently running and stop it,
+# setting the restart flag to ensure we start it up again upon completion.
+if [ "$mysql_slave" == "true" ]; then
+  mysql_slave_restart=false
+  slave_mysql=$("$mysql_cmd" "$mysql_args" -e "SHOW SLAVE STATUS \G")
+  slave_io=$(echo "$slave_mysql" | grep 'Slave_IO_Running:' | awk '{print $2}')
+  slave_sql=$(echo "$slave_mysql" | grep 'Slave_SQL_Running:' | awk '{print $2}')
+  printf "Stop slave ... "
+  if [ "Yes" == "$slave_io" ] || [ "Yes" == "$slave_sql" ]; then
+    "$mysqladmin_cmd" "$mysql_args" start-slave
+    success_or_error
+    mysql_slave_restart=true
+  else
+    message "warn" "Skipped"
+  fi
+fi
+
+printf "Generating database list ... "
+
+# Fetch list of database names, excluding those we do not want to export.
+databases=($("$mysql_cmd" "$mysql_args" --batch --skip-column-names -e 'SHOW DATABASES;' | grep -Ev "^(${mysql_exclude})$"))
+success_or_quit "Failed to export databases"
+
+# Check we have some databases to export, otherwise stop here.
+database_count=${#databases[@]}
+if [ "$database_count" -eq 0 ]; then
+  quit "No databases to export"
+fi
+
+# Print out number of databases and output destination.
+printf "Exporting ${database_count} database";
+if [ $database_count -ne 1 ]; then printf "s"; fi;
+echo " to ${backup_dir}/<db>.sql.gz"
+
+# If GPG is enabled, build the GPG args based on the config options.
+if [ "$gpg_enabled" == true ]; then
+  if [ -z "$gpg_recipient" ]; then
+    quit "'gpg_recipient' must be set if GPG is enabled."
+  fi
+  gpg_command="${gpg_cmd} ${gpg_args} --recipient ${gpg_recipient}"
+  if [ -z "$gpg_sign" ]; then
+    gpg_command+=" --sign"
+    if [ -z "$gpg_signer" ]; then
+      gpg_command+=" --default-key $gpg_signer"
+    fi
+  fi
+fi
+
+# Build the mysqldump command with all flags and output.
+mysql_export_cmd="${mysqldump_cmd} ${mysql_args} ${mysqldump_args} --databases %s"
+if [ "$compress_cmd" ]; then
+  mysql_export_cmd+=" | ${compress_cmd}"
+fi
+if [ "$gpg_enabled" == "true" ]; then
+  mysql_export_cmd+=" | ${gpg_command} --output ${backup_dir}/%s${file_extension}.gpg"
+else
+  mysql_export_cmd+=" > ${backup_dir}/%s${file_extension}"
+fi
+
+# Loop through and create compressed backup files for each database.
+for db in "${databases[@]}"; do
+  printf "┕ "
+  printf "%-30s" "$db" | tr ' ' .
+  printf -v export_cmd "$mysql_export_cmd" "$db" "$db"
+  printf " "
+  eval "$export_cmd 2>&1"
+  success_or_quit
+done
+
+# Build AWS cli options (profile).
+aws_args="--profile $aws_profile"
+
+# Upload all files in backup directory to S3, ensuring the bucket exists
+# and creating it if not. If AWS is disabled, we force set the backup
+# directory removal flag to false.
+if [ "$aws_enabled" == "true" ]; then
+  printf "[Start AWS S3 upload]\n"
+  # Check if bucket exists, if not create it
+  printf "Checking for existing AWS bucket '${aws_bucket}' ... "
+  aws_bucket_check=$("$aws_cmd" s3 ls "s3://${aws_bucket}" $aws_args 2>&1)
+  if [ $? -eq 0 ]; then
+    message "success" # Bucket exists
+  elif [[ "$aws_bucket_check" == *NoSuchBucket* ]]; then
+    message "warn" "No such bucket"
+    printf "Creating AWS S3 bucket ... "
+    $aws_cmd s3 mb "s3://${aws_bucket}" $aws_args >/dev/null 2>&1
+    success_or_quit
+  else
+    quit "Failed to check bucket."
+  fi
+  printf "Uploading ${backup_dir} ... "
+  $aws_cmd s3 cp "$backup_dir" "s3://${aws_bucket}/${aws_dir}" $aws_args --recursive >/dev/null 2>&1
+  success_or_error
+else
+  backup_dir_remove=false
+fi
+
+# Perform cleanup script now all processing is complete.
+cleanup
